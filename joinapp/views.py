45e2801a4b8a41df52a83a3fbc8b2ac960f93147
DESKTOP-H999ON1\user <034179@naver.com>
2021-07-12 21:11:49 +0900

    modify login

---
+++ joinapp/views.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404
+
+# Create your views here.
+from django.urls import reverse
+from django.utils.decorators import method_decorator
+from django.views.generic import RedirectView
+
+from joinapp.models import Join
+from projectapp.models import Project
+
+
+@method_decorator(login_required, 'get')
+class JoinView(RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('projectapp:detail', kwargs={'pk':self.request.GET.get('project_pk')})
+
+    def get(self, request, *args, **kwargs):
+        project = get_object_or_404(Project, pk=self.request.GET.get('project_pk'))
+        user = self.request.user
+        join = Join.objects.filter(user=user, project=project)
+
+        if join.exists():
+            join.delete()
+        else:
+            Join(user=user, project=project).save()
+        return super(JoinView, self).get(request, *args, **kwargs)
