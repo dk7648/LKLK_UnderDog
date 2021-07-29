433527ecea6fcd172f94760e72155df108d074a5
Jihyun-Choi <70438098+Jihyun-Choi@users.noreply.github.com>
2021-07-29 20:27:19 +0900

    modify Project Create

--- projectapp/forms.py
+++ projectapp/forms.py
@@ -8,8 +8,7 @@ from projectapp.models import Project
 class ProjectCreationForm(ModelForm):
     content = forms.CharField(widget=forms.Textarea(attrs={'class': 'editable text-left',
                                                            'style': 'height: auto;'}))
-    project = forms.ModelChoiceField(queryset=Project.objects.all(), required=False)
 
     class Meta:
         model = Project
-        fields = ['title', 'image', 'image2', 'image3', 'project', 'content', 'personnel']
+        fields = ['title', 'image', 'image2', 'image3', 'content', 'personnel']
